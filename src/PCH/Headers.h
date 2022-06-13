1bd307c7dfa59c29ffc818c294041af5f45aa3f9
YB970902 <103626230+ybnamgung-highbrow@users.noreply.github.com>
2022-06-14 01:57:30 +0900

    [feature] include filesystem
    경로 설정을 위한 filesystem 포함

--- src/PCH/Headers.h
+++ src/PCH/Headers.h
@@ -1,11 +1,15 @@
 #pragma once
 
+#include <iostream>
+#include <filesystem>
 #include <gdiplus.h>				// GDI+를 사용하기 위한 헤더파일
 #pragma comment(lib, "gdiplus")		// GDI+를 사용하기 위한 라이브러리 파일
 
 #include "rapidjson/document.h"
 
 using namespace Gdiplus;
+using namespace std;
+using namespace std::filesystem;
 
 #ifdef UNICODE
 #pragma comment(linker, "/entry:wWinMainCRTStartup /subsystem:console")
